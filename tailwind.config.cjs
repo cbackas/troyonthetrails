8bd7215528efd5249064bc00c59947884da78e89
cbackas <zgibson@live.com>
2023-08-19 23:05:00 -0500

    add red marquee when troy is on the trails

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,20 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...fontFamily.sans],
       },
+      animation: {
+        marquee: 'marquee 10s linear infinite',
+        marquee2: 'marquee2 10s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     },
   },
 };
