a44214a480b94dd63b016f6ec58e044e39a0c38f
cbackas <zgibson@live.com>
2024-07-28 11:30:53 -0500

    restructure project to split up services

---
+++ web_service/src/env_utils.rs
@@ -0,0 +1,58 @@
+use std::env;
+
+use tracing::error;
+
+use crate::utils::hash_string;
+
+pub fn get_host_uri() -> String {
+    match env::var("HOST") {
+        Ok(host) => format!("https://{}", host),
+        _ => match env::var("FLY_APP_NAME") {
+            Ok(host) => format!("https://{}.fly.dev", host),
+            _ => {
+                format!("http://localhost:{}", get_port())
+            }
+        },
+    }
+}
+
+pub fn get_port() -> u16 {
+    let default_port: u16 = 8080;
+
+    let port = match env::var("PORT") {
+        Ok(port) => port,
+        _ => default_port.to_string(),
+    };
+    let port: u16 = match port.parse::<_>() {
+        Ok(port) => port,
+        _ => {
+            error!("Failed to parse PORT env var, using default");
+            default_port
+        }
+    };
+
+    port
+}
+
+pub fn get_webhook_secret() -> String {
+    let wh_seed = match env::var("WH_SEED") {
+        Ok(wh_seed) => wh_seed,
+        _ => "defaultwebhookseed".to_string(),
+    };
+
+    hash_string(&wh_seed)[0..32].to_string()
+}
+
+pub fn get_strava_user_id() -> Option<String> {
+    match env::var("STRAVA_USER_ID") {
+        Ok(user_id) => Some(user_id),
+        _ => None,
+    }
+}
+
+pub fn get_db_encryption_key() -> String {
+    match env::var("DB_ENCRYPTION_KEY") {
+        Ok(key) => key,
+        _ => "defaultdbencryptionkey".to_string(),
+    }
+}
