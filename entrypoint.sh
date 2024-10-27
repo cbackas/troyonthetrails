8e5b74d43f2cd383d9845e3e9484dfa29cfe5f3c
cbackas <zgibson@live.com>
2024-10-26 20:55:36 -0500

    launch chromedriver correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [[ $# -gt 0 ]]; then
     # check if the command is "map_service"
     if [ "$1" = "map_service" ]; then
         # run chromium-driver in the background
-        /usr/bin/chromium-driver --no-sandbox --headless --disable-gpu --port=4444 &
+        /usr/bin/chromedriver --no-sandbox --headless --disable-gpu --port=4444 &
     fi
     dumb-init -- "$@"
 fi
