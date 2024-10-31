7034edffc214f7e73b88b8f6dccde754236d5892
cbackas <zgibson@live.com>
2024-10-31 17:02:30 -0500

    start geckodriver in the right place

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 if [[ $# -gt 0 ]]; then
     # check if the command is "map_service"
     if [ "$1" = "map_service" ]; then
-        /usr/bin/geckodriver --port=4444 &
+        /usr/local/bin/geckodriver --port=4444 &
     fi
     dumb-init -- "$@"
 fi
