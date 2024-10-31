47c0a0d1b069c8b92b2303085a7ff8f85756df28
cbackas <zgibson@live.com>
2024-10-31 17:38:41 -0500

    Revert "screw chrome?"
    This reverts commit 82b4ef6839e77559c3a753b91e42ba348d10a306.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 if [[ $# -gt 0 ]]; then
     # check if the command is "map_service"
     if [ "$1" = "map_service" ]; then
-        /usr/local/bin/geckodriver --port=4444 &
+        # run chromium-driver in the background
+        /usr/bin/chromedriver --port=4444 &
     fi
     dumb-init -- "$@"
 fi
