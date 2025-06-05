0ae36937226403fdad7c46f76618664c09469575
cbackas <git@zac.gg>
2025-06-04 20:45:14 -0500

    rough pass at showing stats for each trail in the list

--- trail_service/src/ride_counts.rs
+++ trail_service/src/ride_counts.rs
@@ -2,7 +2,21 @@ use std::collections::HashMap;
 
 use shared_lib::{strava_structs::Activity, trail_structs::TrailSystem, utils};
 
-pub fn calculate_counts(trails: Vec<TrailSystem>, rides: Vec<Activity>) -> HashMap<u64, i32> {
+#[derive(Default, Debug, Clone, Copy, Eq)]
+pub struct TrailStats {
+    pub id: u64,
+    pub rides: i32,
+    pub achievement_count: i64,
+    pub total_moving_time: i64,
+}
+
+impl PartialEq for TrailStats {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id && self.rides == other.rides
+    }
+}
+
+pub fn calculate_stats(trails: Vec<TrailSystem>, rides: Vec<Activity>) -> HashMap<u64, TrailStats> {
     let counts = rides.iter().fold(HashMap::new(), |mut counts, ride| {
         let closest_trail = trails
             .iter()
@@ -13,7 +27,14 @@ pub fn calculate_counts(trails: Vec<TrailSystem>, rides: Vec<Activity>) -> HashM
             .min_by(|a, b| a.1.partial_cmp(&b.1).unwrap_or(std::cmp::Ordering::Equal));
 
         if let Some((id, _)) = closest_trail {
-            *counts.entry(id).or_insert(0) += 1;
+            let entry = counts.entry(id).or_insert(TrailStats {
+                id,
+                ..Default::default()
+            });
+
+            entry.rides += 1;
+            entry.achievement_count += ride.achievement_count;
+            entry.total_moving_time += ride.moving_time;
         }
 
         counts
