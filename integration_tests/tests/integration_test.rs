c8e0d3169e9f809bd1ab32cf7dad4a2150ee50d6
cbackas <git@zac.gg>
2025-08-10 22:13:55 -0500

    get rid of unused code

--- integration_tests/tests/integration_test.rs
+++ integration_tests/tests/integration_test.rs
@@ -1,6 +1,3 @@
-use std::collections::HashMap;
-
-use shared_lib::utils;
 use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -19,24 +16,9 @@ async fn external_test() {
 
     tracing::info!("db ready, fetching rides");
 
-    let rides = strava_service::get_all_activities()
+    let _rides = strava_service::get_all_activities()
         .await
         .expect("Failed to get activities");
 
     tracing::info!("Fetched rides, fetching trails");
-
-    let trails = web_service::trail_lib::get_trail_data()
-        .await
-        .expect("Failed to get trail data");
-
-    tracing::info!("Rides: {:?}, Trails: {:?}", rides.len(), trails.len());
-
-    let counts = web_service::trail_lib::get_trail_ride_count(trails.clone(), rides.clone());
-    let mut sorted_counts: Vec<_> = counts.iter().collect();
-    sorted_counts.sort_by(|a, b| b.1.cmp(a.1));
-
-    for (id, count) in sorted_counts.iter().take(10) {
-        let trail = trails.iter().find(|t| t.id == **id).unwrap();
-        tracing::info!("Trail: {}, Count: {}", trail.name, count);
-    }
 }
--- trail_service/src/lib.rs
+++ trail_service/src/lib.rs
@@ -4,23 +4,3 @@ use shared_lib::{strava_structs::Activity, trail_structs::TrailSystem, utils};
 
 pub mod ride_counts;
 pub mod trail_data;
-
-pub fn get_trail_ride_count(trails: Vec<TrailSystem>, rides: Vec<Activity>) -> HashMap<u64, i32> {
-    let counts = rides.iter().fold(HashMap::new(), |mut counts, ride| {
-        let closest_trail = trails
-            .iter()
-            .filter_map(|trail| {
-                let distance = utils::haversine_distance(ride.clone(), trail.clone()).ok()?;
-                (distance <= 3000.0).then_some((trail.id, distance))
-            })
-            .min_by(|a, b| a.1.partial_cmp(&b.1).unwrap_or(std::cmp::Ordering::Equal));
-
-        if let Some((id, _)) = closest_trail {
-            *counts.entry(id).or_insert(0) += 1;
-        }
-
-        counts
-    });
-
-    counts
-}
