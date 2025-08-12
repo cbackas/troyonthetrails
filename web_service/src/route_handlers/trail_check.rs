ec70dfe7a3426bf2e153de6242bf0a166750f946
cbackas <git@zac.gg>
2025-08-12 04:51:45 -0500

    if stats are already cached then include them on page load

--- web_service/src/route_handlers/trail_check.rs
+++ web_service/src/route_handlers/trail_check.rs
@@ -2,9 +2,29 @@ use shared_lib::trail_structs::{TrailStatus, TrailSystem};
 
 pub async fn handler() -> impl axum::response::IntoResponse {
     let trail_data_cache = trail_service::trail_data::get_data().await;
-    super::html_template::HtmlTemplate(TrailCheckTemplate {
-        trails: trail_data_cache.trail_data,
-    })
+    let trails = match strava_service::get_cached_activities().await {
+        None => trail_data_cache.trail_data,
+        Some(rides) => {
+            let trail_stats = trail_service::ride_counts::calculate_stats(
+                trail_data_cache.trail_data.clone(),
+                rides,
+            );
+
+            trail_data_cache
+                .trail_data
+                .into_iter()
+                .map(|mut trail| {
+                    if let Some(stats) = trail_stats.get(&trail.id) {
+                        trail.stats = Some((*stats).into());
+                    }
+                    trail
+                })
+                .filter(|trail| trail.status != TrailStatus::Unknown)
+                .collect::<Vec<TrailSystem>>()
+        }
+    };
+
+    super::html_template::HtmlTemplate(TrailCheckTemplate { trails })
 }
 
 #[derive(askama::Template)]
